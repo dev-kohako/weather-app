46f207fb5a457ee134932a2984169f11a35383cf
kohako_dev <kohako.tech@gmail.com>
2025-02-10 15:49:55 -0300

    feat: apollo client configuration in app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,26 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+
+function App({ Component, pageProps }: AppProps) {
+  const client = new ApolloClient({
+    uri: 'http://localhost:4000',
+    cache: new InMemoryCache(),
+    defaultOptions: {
+      watchQuery: {
+        fetchPolicy: "network-only",
+      },
+      query: {
+        fetchPolicy: "network-only",
+      },
+    },
+  })
+
+  return (
+    <ApolloProvider client={client}>
+        <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
+
+export default App
